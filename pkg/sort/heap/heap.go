1b407bd4124a4e8cb21474b3d817d747bfa5d95c
caiflower <caif1ower@163.com>
2024-01-19 15:03:21 +0800

    公开struct，修复如果Pop和Add方法交替执行时，产生的bug

--- pkg/sort/heap/heap.go
+++ pkg/sort/heap/heap.go
@@ -11,16 +11,20 @@ type Api interface {
 	IsEmpty() bool
 }
 
-type heap[T int | int8 | int16 | int32 | int64 | float32 | float64] struct {
+type Heap[T int | int8 | int16 | int32 | int64 | float32 | float64] struct {
 	common[T]
 }
 
-func NewTopMin[T int | int8 | int16 | int32 | int64 | float32 | float64]() *heap[T] {
-	return &heap[T]{common: common[T]{size: 0, arr: make([]T, 0)}}
+func NewTopMin[T int | int8 | int16 | int32 | int64 | float32 | float64]() *Heap[T] {
+	return &Heap[T]{common: common[T]{size: 0, arr: make([]T, 0)}}
 }
 
-func (h *heap[T]) Add(val T) {
-	h.arr = append(h.arr, val)
+func (h *Heap[T]) Add(val T) {
+	if len(h.arr) > h.size {
+		h.arr[h.size] = val
+	} else {
+		h.arr = append(h.arr, val)
+	}
 	h.size++
 	i := h.size - 1
 	for i > 0 {
@@ -34,7 +38,7 @@ func (h *heap[T]) Add(val T) {
 	}
 }
 
-func (h *heap[T]) Pop() (T, error) {
+func (h *Heap[T]) Pop() (T, error) {
 	if h.size == 0 {
 		return 0, errors.New("empty heap")
 	} else {
@@ -69,16 +73,20 @@ func (h *heap[T]) Pop() (T, error) {
 	}
 }
 
-type maxHeap[T int | int8 | int16 | int32 | int64 | float32 | float64] struct {
+type MaxHeap[T int | int8 | int16 | int32 | int64 | float32 | float64] struct {
 	common[T]
 }
 
-func NewTopMax[T int | int8 | int16 | int32 | int64 | float32 | float64]() *maxHeap[T] {
-	return &maxHeap[T]{common: common[T]{size: 0, arr: make([]T, 0)}}
+func NewTopMax[T int | int8 | int16 | int32 | int64 | float32 | float64]() *MaxHeap[T] {
+	return &MaxHeap[T]{common: common[T]{size: 0, arr: make([]T, 0)}}
 }
 
-func (h *maxHeap[T]) Add(val T) {
-	h.arr = append(h.arr, val)
+func (h *MaxHeap[T]) Add(val T) {
+	if len(h.arr) > h.size {
+		h.arr[h.size] = val
+	} else {
+		h.arr = append(h.arr, val)
+	}
 	h.size++
 	i := h.size - 1
 	for i > 0 {
@@ -92,7 +100,7 @@ func (h *maxHeap[T]) Add(val T) {
 	}
 }
 
-func (h *maxHeap[T]) Pop() (T, error) {
+func (h *MaxHeap[T]) Pop() (T, error) {
 	if h.size == 0 {
 		return 0, errors.New("empty heap")
 	} else {
