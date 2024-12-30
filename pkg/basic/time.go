544acbfaf13d45dbc407b1313327666e001a3a51
caiflower <caif1ower@163.com>
2024-12-30 20:58:53 +0800

    feature(time/time_standard): fix json format

--- pkg/basic/time.go
+++ pkg/basic/time.go
@@ -105,9 +105,9 @@ func (t *Time) MarshalJSON() ([]byte, error) {
 	if t.IsZero() {
 		return []byte("\"\""), nil
 	}
-	b := make([]byte, 0, len(TimeFormatT)+2)
+	b := make([]byte, 0, len(TimeFormat)+2)
 	b = append(b, '"')
-	b = t.Time().AppendFormat(b, TimeFormatT)
+	b = t.Time().AppendFormat(b, TimeFormat)
 	b = append(b, '"')
 	return b, nil
 }
