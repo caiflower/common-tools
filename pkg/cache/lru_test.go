3e0d7f15aa22a3b83a5a6dedb642e39da62e3660
caiflower <caif1ower@163.com>
2025-02-28 18:24:28 +0800

    feature(basic): lru

---
+++ pkg/cache/lru_test.go
@@ -0,0 +1,33 @@
+package cache
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestNewLRUCache(t *testing.T) {
+	cache := NewLRUCache(2)
+	go cache.Put("key1", "value1")
+	go cache.Put("key2", "value2")
+	go cache.Put("key3", "value3")
+	go cache.Put("key1", "value1")
+	fmt.Println(cache)
+
+	cache1 := NewLRUCache(3)
+	go cache1.Put("key1", "value1")
+	go cache1.Put("key2", "value2")
+	go cache1.Put("key3", "value3")
+	go cache1.Put("key2", "value2")
+	go cache1.Put("key1", "value1")
+	go cache1.Put("key3", "value3")
+
+	time.Sleep(1 * time.Second)
+
+	for i := 1; i <= 3; i++ {
+		go fmt.Println(cache1.Get(fmt.Sprintf("key%d", i)))
+		go fmt.Println(cache1.Size())
+	}
+
+	time.Sleep(1 * time.Second)
+}
