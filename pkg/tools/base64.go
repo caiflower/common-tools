b9800ab83ed389c055f670f44d7e792a710ef70a
caiflower <caif1ower@163.com>
2025-03-28 20:20:28 +0800

    fix(tools): base64fix
    (cherry picked from commit 2c5ba8bd9044dc0a1d272066ac1fd72a0f85c09d)

--- pkg/tools/base64.go
+++ pkg/tools/base64.go
@@ -3,6 +3,5 @@ package tools
 import "encoding/base64"
 
 func ToBase64(str string) string {
-	encoding := base64.Encoding{}
-	return encoding.EncodeToString([]byte(str))
+	return base64.StdEncoding.EncodeToString([]byte(str))
 }
