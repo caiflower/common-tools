a1ae71ac5fd9f12ad4ffa8f488160463802107a2
caiflower <caif1ower@163.com>
2025-04-06 12:25:59 +0800

    fix(crontab): on panic

--- pkg/crontab/regular_job_test.go
+++ pkg/crontab/regular_job_test.go
@@ -19,3 +19,16 @@ func TestRegularJob(t *testing.T) {
 	time.Sleep(time.Second * 10)
 	job.Stop()
 }
+
+func TestPanicRegularJob(t *testing.T) {
+	fn := func() {
+		panic("test panic")
+	}
+
+	job := NewRegularJob("testRegularJob", fn, WithInterval(time.Second*2), WithIgnorePanic())
+
+	job.Run()
+
+	time.Sleep(time.Second * 10)
+	job.Stop()
+}
