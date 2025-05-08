3c4f04680dfb9440e95ebbdb288efd4ec2d1bfd1
caiflower <caif1ower@163.com>
2025-05-08 16:59:36 +0800

    feature(redisv1): keyPrefix

--- redis/v1/client_test.go
+++ redis/v1/client_test.go
@@ -1,7 +1,7 @@
 package redisv1
 
 import (
-	"fmt"
+	"github.com/google/go-cmp/cmp"
 	"testing"
 	"time"
 )
@@ -9,6 +9,123 @@ import (
 type TestObject struct {
 	Name string
 	Age  int
+}
+
+func commonTestcase(client RedisClient) {
+	if err := client.Set("test", "test"); err != nil {
+		panic(err)
+	}
+
+	if getString, err := client.GetString("test"); err != nil {
+		panic(err)
+	} else {
+		assert(getString == "test")
+	}
+
+	if err := client.Del("test"); err != nil {
+		panic(err)
+	}
+
+	object := &TestObject{Age: 1, Name: "testObject"}
+	if err := client.Set("object", object); err != nil {
+		panic(err)
+	}
+
+	object1 := &TestObject{}
+	if err := client.Get("object", object1); err != nil {
+		panic(err)
+	} else {
+		assert(cmp.Equal(object, object1))
+	}
+
+	if err := client.Del("object"); err != nil {
+		panic(err)
+	}
+
+	if err := client.SetPeriod("objectTTL", &TestObject{Age: 1, Name: "testObject"}, time.Second*60); err != nil {
+		panic(err)
+	}
+
+	if err := client.HSet("ObjectH", "key", object); err != nil {
+		panic(err)
+	}
+
+	if err := client.HSet("ObjectH", map[string]interface{}{"key1": object}); err != nil {
+		panic(err)
+	}
+
+	if err := client.HSet("ObjectH", map[string]string{"key2": "key2string"}); err != nil {
+		panic(err)
+	}
+
+	if err := client.HGet("ObjectH", "key", object1); err != nil {
+		panic(err)
+	} else {
+		assert(cmp.Equal(object, object1))
+	}
+
+	if err := client.HGet("ObjectH", "key1", object1); err != nil {
+		panic(err)
+	} else {
+		assert(cmp.Equal(object, object1))
+	}
+
+	if v, err := client.HGetString("ObjectH", "key2"); err != nil {
+		panic(err)
+	} else {
+		assert(v == "key2string")
+	}
+
+	if err := client.Del("ObjectH"); err != nil {
+		panic(err)
+	}
+
+	m := make(map[string]interface{})
+	m["mSetKey1"] = "MSetValue1"
+	m["mSetKey2"] = object
+	if err := client.MSet(m); err != nil {
+		panic(err)
+	}
+
+	if getString, err := client.GetString("mSetKey1"); err != nil {
+		panic(err)
+	} else {
+		assert(getString == "MSetValue1")
+	}
+
+	if err := client.Get("mSetKey2", object1); err != nil {
+		panic(err)
+	} else {
+		assert(cmp.Equal(object, object1))
+	}
+
+	if err := client.Del("mSetKey1", "mSetKey2"); err != nil {
+		panic(err)
+	}
+
+	m1 := make(map[string]string)
+	m1["mSetKey1"] = "MSetValue1"
+	m1["mSetKey2"] = "MSetValue2"
+
+	if err := client.MSet(m1); err != nil {
+		panic(err)
+	}
+
+	if getString, err := client.GetString("mSetKey1"); err != nil {
+		panic(err)
+	} else {
+		assert(getString == "MSetValue1")
+	}
+
+	if getString, err := client.GetString("mSetKey1"); err != nil {
+		panic(err)
+	} else {
+		assert(getString == "MSetValue1")
+	}
+
+	if err := client.Del("mSetKey1", "mSetKey2"); err != nil {
+		panic(err)
+	}
 }
 
 func TestNewRedisClient(t *testing.T) {
@@ -21,70 +138,25 @@ func TestNewRedisClient(t *testing.T) {
 		panic("client init failed")
 	}
 
-	if err := client.Set("test", "test"); err != nil {
-		panic(err)
+	commonTestcase(client)
+}
+
+func TestNewRedisClientWithPrefix(t *testing.T) {
+	client := NewRedisClient(Config{
+		Addrs:     []string{"10.226.138.162:6379"},
+		Password:  "CloudIaas@123",
+		DB:        8,
+		KeyPrefix: "test:",
+	})
+	if client == nil {
+		panic("client init failed")
 	}
 
-	if getString, err := client.GetString("test"); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(getString)
-	}
+	commonTestcase(client)
+}
 
-	if err := client.Del("test"); err != nil {
-		panic(err)
-	}
-
-	if err := client.Set("object", &TestObject{Age: 1, Name: "testObject"}); err != nil {
-		panic(err)
-	}
-
-	object := &TestObject{}
-	if err := client.Get("object", object); err != nil {
-		panic(err)
-	} else {
-		fmt.Printf("object = %v\n", object)
-	}
-
-	if err := client.Del("object"); err != nil {
-		panic(err)
-	}
-
-	if err := client.SetPeriod("objectTTL", &TestObject{Age: 1, Name: "testObject"}, time.Second*60); err != nil {
-		panic(err)
-	}
-
-	if err := client.HSet("ObjectH", "key", &TestObject{Age: 1, Name: "testObject"}); err != nil {
-		panic(err)
-	}
-
-	if err := client.HSet("ObjectH", map[string]interface{}{"key1": &TestObject{Age: 1, Name: "testObject"}}); err != nil {
-		panic(err)
-	}
-
-	if err := client.HSet("ObjectH", map[string]string{"key2": "string"}); err != nil {
-		panic(err)
-	}
-
-	if err := client.HGet("ObjectH", "key", object); err != nil {
-		panic(err)
-	} else {
-		fmt.Printf("object = %v\n", object)
-	}
-
-	if err := client.HGet("ObjectH", "key1", object); err != nil {
-		panic(err)
-	} else {
-		fmt.Printf("object = %v\n", object)
-	}
-
-	if v, err := client.HGetString("ObjectH", "key2"); err != nil {
-		panic(err)
-	} else {
-		fmt.Printf("string = %v\n", v)
-	}
-
-	if err := client.Del("ObjectH"); err != nil {
-		panic(err)
+func assert(success bool) {
+	if !success {
+		panic("test failed.")
 	}
 }
