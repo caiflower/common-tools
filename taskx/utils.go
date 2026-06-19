8bf65e76b33d4c6ae270edece593529a807d7c53
caiflower <caif1ower@163.com>
2026-06-19 13:49:24 +0800

    migrate

--- taskx/utils.go
+++
@@ -1,9 +0,0 @@
-package taskx
-
-func isFinished(state string) bool {
-	return state == TaskFailed || state == TaskSucceeded
-}
-
-func isRollbackFinished(state string) bool {
-	return state == string(RollbackFailed) || state == string(RollbackSucceeded) || state == string(NoneRollback)
-}
